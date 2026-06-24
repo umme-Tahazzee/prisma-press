1f6405ced71a7e4321d79f161282c77e27d65825
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-06-24 15:07:58 +0600

    feat: Configuring Environment Variables With dotenv And Using Express Built In Middlwares In app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import config from "./config/index.js";
 
 const app: Application = express();
+
+
+app.use(cors({
+     origin: config.app_url,
+     credentials : true
+}))
+app.use(express.json())
+express.urlencoded({extended: true})
+app.use(cookieParser())
+
 app.get('/',(req: Request, res: Response)=>{
       res.send("Hello world")
 })
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import app from "./app.js";
 import { prisma } from "./lib/prisma.js";
+import config from "./config/index.js";
 
 
 
-const PORT = process.env.PORT
+const PORT = config.port
 
 async function main() {
     try {
