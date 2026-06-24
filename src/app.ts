75f1deb0f9129184167ca39b05744f0951bcb8e9
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-06-24 16:21:13 +0600

    Setting Up Multi Prisma Schema File And Defining User Model And Profile Model

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,24 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from 'cors'
 import config from "./config/index.js";
+import { prisma } from "./lib/prisma.js";
 
 const app: Application = express();
 
 
 app.use(cors({
-     origin: config.app_url,
-     credentials : true
+    origin: config.app_url,
+    credentials: true
 }))
 app.use(express.json())
-express.urlencoded({extended: true})
+express.urlencoded({ extended: true })
 app.use(cookieParser())
 
-app.get('/',(req: Request, res: Response)=>{
-      res.send("Hello world")
+app.get('/', (req: Request, res: Response) => {
+    const user = prisma.user.findMany()
+    console.log(user);
+
+    res.send("Hello world")
 })
 
 export default app;
