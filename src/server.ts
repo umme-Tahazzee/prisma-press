02ece4c61617ee54f95265b4ef9b304a0f1cc8cc
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-06-24 12:46:51 +0600

    feat : Setting Up server.ts, app.ts, files with express

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
-import app from "./app.js"
+import "dotenv/config";
+import app from "./app.js";
+import { prisma } from "./lib/prisma.js";
 
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT
 
 async function main() {
     try {
+        await prisma.$connect();
+        console.log("Connected to DB successfully");
+
         app.listen(PORT, () => {
-            console.log(`Server is running on ${PORT}`)
-        })
+            console.log(`Server is running on ${PORT}`);
+        });
     } catch (error) {
-        console.log('Error starting the server', error)
+        console.log("Error starting the server", error);
+        await prisma.$disconnect();
+        process.exit(1);
     }
 }
 
-main()
+main();
