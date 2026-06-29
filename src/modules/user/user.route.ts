eed3848838c0ce1b8255fd576ab4d81284f19e17
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-06-29 20:42:29 +0600

    feat : fix baerer token

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -26,10 +26,11 @@ router.post("/register", userController.registerUser);
 const auth = (...requireRole: UserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token =
+      req.cookies.accessToken ? 
       req.cookies.accessToken
-    // || req.headers.authorization?.startsWith("Bearer")
-    //   ? req.headers.authorization?.split(" ")[1]
-    //   : req.headers.authorization;
+      :req.headers.authorization?.startsWith("Bearer")
+      ? req.headers.authorization?.split(" ")[1]
+      : req.headers.authorization 
 
     if (!token) {
       throw new Error("You are not loged out");
