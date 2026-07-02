d982db075c8739592b221811948966b479e39ded
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-07-02 19:21:36 +0600

    feat : implement comment update

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -19,15 +19,40 @@ const createComment = catchAsync(async(req:Request, res:Response)=>{
 })
 
 const getAllComments = catchAsync(async(req:Request, res:Response) => {
-     
+       const result = await commentService.getAllCommentFromDb() 
+
+        sendResponse(res, {
+              success : true,
+              statusCode : httpStatus.OK,
+              message : "get all comment succesfully",
+              data : result
+      })
 })
 
 const getCommentsById = catchAsync(async(req:Request, res:Response) => {
+     const authorId = req.params.authorId as string
 
+     const result = await commentService.getAllCommentFromByDb(authorId)
+     sendResponse(res, {
+              success : true,
+              statusCode : httpStatus.OK,
+              message : `get ${authorId} comments successfully`,
+              data : result
+      })
 })
 
 const updateComments = catchAsync(async(req:Request, res:Response) => {
-
+    const authorId = req.user?.id as string
+    const commentId = req.params.commentId as string
+    const data = req.body
+    const result = await commentService.updateCommentsFromDb(commentId , authorId, data)
+     sendResponse(res, {
+              success : true,
+              statusCode : httpStatus.OK,
+              message : `update ${authorId} comments successfully`,
+              data : result
+      })
+    
 })
 
 
--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,28 +1,82 @@
 import { prisma } from "../../lib/prisma";
 import { ICreateCommentPayload } from "./comments.interface";
 
-const createComment = async(authorId : string, payload:ICreateCommentPayload) => {
-     
-       console.log(payload.postId);
-     
-   
+const createComment = async (authorId: string, payload: ICreateCommentPayload) => {
+
      await prisma.post.findUniqueOrThrow({
-            where :{
-                 id : payload.postId
-            }
+          where: {
+               id: payload.postId
+          }
      })
- 
-       
+
+
      const comment = await prisma.comment.create({
           data: {
                ...payload,
-                authorId : authorId
+               authorId: authorId
           },
      });
 
      return comment
 }
 
+const getAllCommentFromDb = async () => {
+     const result = await prisma.comment.findMany()
+     return result
+}
+
+const getAllCommentFromByDb = async (authorId: string) => {
+     const comments = await prisma.comment.findUniqueOrThrow({
+          where: {
+               id: authorId
+          },
+          include: {
+               post: {
+                    select: {
+                         id: true,
+                         title: true,
+                         views: true
+                    }
+               }
+
+          }
+
+     })
+     return comments
+}
+
+const updateCommentsFromDb = async (commentId: string, authorId: string, data: ICreateCommentPayload) => {
+     const commentData = await prisma.comment.findUniqueOrThrow({
+          where: {
+               id: commentId,
+               authorId
+          },
+          select: {
+               id: true
+          }
+     })
+
+     if (!commentData) {
+          throw new Error("Your provide input is not valid")
+     }
+
+     const comment = await prisma.comment.update({
+          where: {
+               id: commentId,
+               authorId
+          },
+          data
+
+     })
+     return comment
+
+}
+
+
+
 export const commentService = {
-      createComment
+     createComment,
+     getAllCommentFromDb,
+     getAllCommentFromByDb,
+     updateCommentsFromDb
 }
